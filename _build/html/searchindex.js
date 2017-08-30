a6da8d280d9c7dc4c92b4654308139e74bfc5bdd
Unknown <vzttzv@gmail.com>
2017-08-30 15:21:05 +0700

    try remove build folder

--- _build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to Translucent Image\u2019s documentation!"],titleterms:{document:0,imag:0,indic:0,tabl:0,transluc:0,welcom:0}})
